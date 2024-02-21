9600f8ffabccfcab6367476ce0668f50aab3072f
m-aleksandra <100863656+m-aleksandra@users.noreply.github.com>
2024-02-22 00:06:42 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from dijkstra import Dijkstra
-from graph_examples import big_graph, positions_big
+from graph_examples import big_graph, positions_big, small_graph, positions_small
 
-D = Dijkstra(big_graph, positions_big, 1)
-D.dijkstra(True)
-D.recreate_path(10)
+
+dijkstra_vis = Dijkstra(big_graph, positions_big, 1)
+print(dijkstra_vis.dijkstra())
+print(dijkstra_vis.recreate_path(15))
+print(dijkstra_vis.recreate_path(16))
+print(dijkstra_vis.recreate_path(10))
+print(dijkstra_vis.recreate_path(18))
